73065154c9bd74665788db909dfbb8927dcdc438
Satya <kimblylabs@gmail.com>
2024-06-03 08:05:35 +0530

    restored

--- day_start_async.py
+++ day_start_async.py
@@ -882,14 +882,14 @@ async def main():
     #df_all_stocks = await db.get_download_symbols_to_trade()
     # await download_ohlc_2min(df_all_stocks)
     # result, error, count_symbol = await process_min_PSAR(df_all_stocks, 'minute')
-    result, error, count_symbol = await process_min_PSAR(df_all_stocks, '2minute')
+    #result, error, count_symbol = await process_min_PSAR(df_all_stocks, '2minute')
     # result, error, count_symbol = await process_min_PSAR(df_all_stocks, '3minute')
     # result, error, count_symbol = await process_min_PSAR(df_all_stocks, '5minute')
     # result, error, count_symbol = await process_min_PSAR(df_all_stocks, '10minute')
     # result, error, count_symbol = await process_min_PSAR(df_all_stocks, '15minute')
     # result, error, count_symbol = await process_min_PSAR(df_all_stocks, '30minute')
     # result, error, count_symbol = await process_min_PSAR(df_all_stocks, '60minute')
-    return
+    #return
     df = await db.get_pre_market_steps()
     if datetime.now().hour > 16:
         df = await db.get_pre_market_steps_ignore_date()
