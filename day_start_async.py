8eb2e725185309beab0cd387f65adf136c2dd1c0
Satya <kimblylabs@gmail.com>
2024-08-24 12:23:55 +0530

    temp

--- day_start_async.py
+++ day_start_async.py
@@ -846,8 +846,9 @@ async def main():
     else:
         print('Error getting NIFTY data from Zerodha')
         await db.pre_process_logs(datetime.now().strftime("%Y-%m-%d"), 'test zerodha', 'zerodha_last_trans', Error, 4)
-   
-    #return
+    
+    await download_ohlc_v2(df_all_stocks, 'minute')
+    return
     df = await db.get_pre_market_steps()
     print('now hour: ', datetime.now().hour)
     if datetime.now().hour >= 16:
