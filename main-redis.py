90a91df0b8bad25425c0e27b12f4b91463a54d8b
Satya <kimblylabs@gmail.com>
2024-09-20 09:46:55 +0530

    date format bug

--- main-redis.py
+++ main-redis.py
@@ -108,7 +108,8 @@ class Start(object):
 
     async def process_alert(self, exchange_code, scanID, alert_timestamp, LRL_value, lrcangletype, lrcanglestart, lrcangleend, angle_degrees, crossover_index, psar_signal, candle_color, high_ha, digit_name, conditionID, r):
         alert_timestamp_str = str(alert_timestamp)  
-        alert_timestamp_dt = datetime.strptime(alert_timestamp_str, "%Y-%m-%d %H:%M:%S")
+        #alert_timestamp_dt = datetime.strptime(alert_timestamp_str, "%Y-%m-%d %H:%M:%S")
+        alert_timestamp_dt = datetime.strptime(alert_timestamp_str, "%Y-%m-%dT%H:%M:%S.%f")
         print('alert_timestamp type:', type(alert_timestamp))
         print('alert_timestamp_dt', alert_timestamp_dt)
         if lrcangletype == 'custom' and lrcanglestart < angle_degrees < lrcangleend:
